031e4e15ce29c5543cae6e0350f464bc5a84a21f
WeiqinDing <wd2m18@soton.ac.uk>
2018-11-21 06:31:56 +0000

    testing Pycharm connection to Github

---
+++ Service/testFile.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File Name : testFile.py
+# Author: Weiqin Ding
+# Mail : wd2m18@soton.ac.uk
+# Created Time: 2018/11/21 6:30 AM
+
+__author__ = 'Ding W.'
